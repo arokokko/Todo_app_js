abd42dac3b519b61d47027e4ca1eb2e87fcc9d3f
arokokko <56476810+arokokko@users.noreply.github.com>
2021-03-29 20:31:53 +0700

    Add ability to change theme of app

--- app.js
+++ app.js
@@ -32,16 +32,85 @@ const tasks = [
 ];
 
 (function (arrOfTasks) {
+
     const objOfTasks = arrOfTasks.reduce((acc, task) => {
         acc[task._id] = task;
         return acc;
     }, {});
+
+    const themes = {
+        default: {
+          '--base-text-color': '#212529',
+          '--header-bg': '#007bff',
+          '--header-text-color': '#fff',
+          '--default-btn-bg': '#007bff',
+          '--default-btn-text-color': '#fff',
+          '--default-btn-hover-bg': '#0069d9',
+          '--default-btn-border-color': '#0069d9',
+          '--danger-btn-bg': '#dc3545',
+          '--danger-btn-text-color': '#fff',
+          '--danger-btn-hover-bg': '#bd2130',
+          '--danger-btn-border-color': '#dc3545',
+          '--input-border-color': '#ced4da',
+          '--input-bg-color': '#fff',
+          '--input-text-color': '#495057',
+          '--input-focus-bg-color': '#fff',
+          '--input-focus-text-color': '#495057',
+          '--input-focus-border-color': '#80bdff',
+          '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+        },
+        dark: {
+          '--base-text-color': '#212529',
+          '--header-bg': '#343a40',
+          '--header-text-color': '#fff',
+          '--default-btn-bg': '#58616b',
+          '--default-btn-text-color': '#fff',
+          '--default-btn-hover-bg': '#292d31',
+          '--default-btn-border-color': '#343a40',
+          '--default-btn-focus-box-shadow':
+            '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+          '--danger-btn-bg': '#b52d3a',
+          '--danger-btn-text-color': '#fff',
+          '--danger-btn-hover-bg': '#88222c',
+          '--danger-btn-border-color': '#88222c',
+          '--input-border-color': '#ced4da',
+          '--input-bg-color': '#fff',
+          '--input-text-color': '#495057',
+          '--input-focus-bg-color': '#fff',
+          '--input-focus-text-color': '#495057',
+          '--input-focus-border-color': '#78818a',
+          '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+        },
+        light: {
+          '--base-text-color': '#212529',
+          '--header-bg': '#fff',
+          '--header-text-color': '#212529',
+          '--default-btn-bg': '#fff',
+          '--default-btn-text-color': '#212529',
+          '--default-btn-hover-bg': '#e8e7e7',
+          '--default-btn-border-color': '#343a40',
+          '--default-btn-focus-box-shadow':
+            '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+          '--danger-btn-bg': '#f1b5bb',
+          '--danger-btn-text-color': '#212529',
+          '--danger-btn-hover-bg': '#ef808a',
+          '--danger-btn-border-color': '#e2818a',
+          '--input-border-color': '#ced4da',
+          '--input-bg-color': '#fff',
+          '--input-text-color': '#495057',
+          '--input-focus-bg-color': '#fff',
+          '--input-focus-text-color': '#495057',
+          '--input-focus-border-color': '#78818a',
+          '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+        },
+    };
 
     let showOnlyCompleted = false;
     
     const listContainer = document.querySelector('.tasks-list-section .list-group');
     const form = document.forms['addTask'];
     const btnsList = document.querySelector('.buttons-list');
+    const themeSelect = document.querySelector('#themeSelect');
 
     // events
     renderAllTasks(objOfTasks);
@@ -49,6 +118,7 @@ const tasks = [
     listContainer.addEventListener('click', onDeleteHandler);
     listContainer.addEventListener('click', onCompleteHandler);
     btnsList.addEventListener('click', onShowingTasksHandler);
+    themeSelect.addEventListener('change', onThemeSelectHandler);
 
     // functions
 
@@ -63,7 +133,7 @@ const tasks = [
     }
 
     // sort array of tasks 
-    function byField(field) {
+    function sortByField(field) {
         return (a, b) => a[field] > b[field] ? 1 : -1;
     }
     
@@ -71,7 +141,7 @@ const tasks = [
     function renderAllTasks(tasksList) {
 
         const fragment = document.createDocumentFragment();
-        const sortedArrayFromObject = Object.values(tasksList).sort(byField('completed'));
+        const sortedArrayFromObject = Object.values(tasksList).sort(sortByField('completed'));
         let arrayOfTasks = showOnlyCompleted ? sortedArrayFromObject.filter(item => !item.completed) : sortedArrayFromObject;
         
         if (!arrayOfTasks.length) {
@@ -113,8 +183,8 @@ const tasks = [
                 <button class="btn btn-info ml-auto complete-btn">Mark as completed</button>`; 
         } else {
             li.innerHTML +=
-            `<p class="mt-2 w-100" style="text-decoration:line-through">${body}</p>   
-            <button class="btn btn-info ml-auto complete-btn">Recover task</button>`;
+                `<p class="mt-2 w-100" style="text-decoration:line-through">${body}</p>   
+                <button class="btn btn-info ml-auto complete-btn">Recover task</button>`;
         }
         
         return li;
@@ -163,6 +233,14 @@ const tasks = [
             objOfTasks[id].completed = !objOfTasks[id].completed;
             renderAllTasks(objOfTasks);
         }
+    }
+
+    // handler select theme
+    function onThemeSelectHandler() {
+        const selectedThemeObject = themes[themeSelect.value];
+        Object.entries(selectedThemeObject).forEach(([key, value]) => {
+            document.documentElement.style.setProperty(key, value);
+        });
     }
 
 })(tasks);
